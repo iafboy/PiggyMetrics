20b55b975308c84db22208bfd1fdaa4a6ceeaf28
popolong <wingman_60_0@163.com>
2017-12-13 13:42:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@ export CONFIG_SERVICE_PASSWORD=oracle
 export NOTIFICATION_SERVICE_PASSWORD=oracle
 export STATISTICS_SERVICE_PASSWORD=oracle
 export ACCOUNT_SERVICE_PASSWORD=oracle
+export LICENSE_SERVICE_PASSWORD=oracle
 export MONGODB_PASSWORD=oracle
 mvn clean package -DskipTests
 docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
